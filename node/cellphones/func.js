41ba76ec94946cbf9e6c244a6aeb96eb117d72d7
shay ben asaf <shay@shays-MacBook-Pro.local>
2016-07-25 16:22:24 +0300

    correct second step

--- node/cellphones/func.js
+++ node/cellphones/func.js
@@ -9,6 +9,7 @@ function myread(filename, x1, y1, x2, y2, callback) {
 //check if the path is wrong
             var error = new Error(err);
             callback(error);
+            return;
         }
         else {
             var antennas = JSON.parse(result);
@@ -21,10 +22,10 @@ function myread(filename, x1, y1, x2, y2, callback) {
                 answer.push(antennas[ann]);
             }
             callback(null, antennas);
+            return;
         }
     });
 }
-7
 
 function distance(x1, y1, x2, y2) {
     return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2));
@@ -37,7 +38,6 @@ function sortarray(array) {
     })
     return array;
 }
-
 
 function phase1(filename, dot1, dot2, callback) {
 
@@ -52,6 +52,7 @@ function phase1(filename, dot1, dot2, callback) {
 //check if the path is wrong
             var error = new Error(err);
             callback(error);
+            return;
         }
         else {
             //ann array with id and distance
@@ -75,18 +76,19 @@ function phase1(filename, dot1, dot2, callback) {
             //in case its the same antenna
             if (antenna1[0] === antenna2[0]) {
                 callback(null, [antenna1[0]]);
+                return;
             }
             else {
                 var result = [antenna1[0], antenna2[0]];
                 callback(null, result);
+                return;
             }
         }
     });
 }
 
-
-function phase2(filename,dot1,dot2, callback) {
-
+function phase2(filename, dot1, dot2, callback) {
+//take take the dot
     var x1 = dot1.x;
     var y1 = dot1.y;
     var x2 = dot2.x;
@@ -98,6 +100,7 @@ function phase2(filename,dot1,dot2, callback) {
 //check if the path is wrong
             var error = new Error(err);
             callback(error);
+            return;
         }
         else {
             //ann array with id and distance
@@ -109,7 +112,15 @@ function phase2(filename,dot1,dot2, callback) {
             for (var i = 0; i < antennas.length; i++) {
                 antennas[i].distance1 = distance(antennas[i].position.x, antennas[i].position.y, x1, y1);
                 antennas[i].distance2 = distance(antennas[i].position.x, antennas[i].position.y, x2, y2);
-                //enters only the relevants antennas for each cellphone
+
+                //*** add line about the checking if it is the same one
+
+                //in case its the same antenna
+                if (antennas[i].distance1 <= antennas[i].transmissionLength && antennas[i].distance2 <= antennas[i].transmissionLength) {
+                    callback(null, [antennas[i].id]);
+                    return;
+                }
+//enters only the relevants antennas for each cellphone
                 if (antennas[i].distance1 <= antennas[i].transmissionLength);
                 {
                     antennas1.push(antennas[i]);
@@ -123,49 +134,55 @@ function phase2(filename,dot1,dot2, callback) {
 //if one of the cellphone dont have relavant antenna
                 var error = new Error(err);
                 callback(error);
+                return;
             }
             else {
-
-                //in case its the same antenna
-                if (antennas1[0].id === antennas2[0].id) {
-                    callback(null, [antennas1[0].id]);
+                //antennas1 = sortarray(antennas1);
+                //antennas2 = sortarray(antennas2);
+                for (var i = 0; i < antennas1.length; i++) {
+                    for (var j = 0; j < antennas2.length; j++) {
+//not the same antenna and the sum of transmissionLengths is bigger than the distance between them
+                        if (antennas1[i].id != antennas2[j].id && antennas1[i].transmissionLength + antennas2[j].transmissionLength >= distance(antennas1[i].position.x, antennas1[i].position.y, antennas2[j].position.x, antennas2[j].position.y)) {
+                            callback(null, [antennas1[i].id, antennas2[j].id]);
+                            return;
+                        }
+                    }
                 }
-                else {
+//nothing found
+                throw new Error("no antennas in range");
 
-                    antennas1 = sortarray(antennas1);
-                    antennas2 = sortarray(antennas2);
 
-                    callback(null, checkrange(antennas1, antennas2));
-                }
             }
         }
     });
 }
 
-function checkrange(arr1, arr2) {
-    var index1 = 0;
-    var index2 = 0;
-    //var result=[];
+/*
+ function checkrange(arr1, arr2) {
+ var index1 = 0;
+ var index2 = 0;
+ //var result=[];
 
-    while (index1 < arr1.length && index2 < arr2.length) {
-        if (arr1[index1].transmissionLength + arr2[index2].transmissionLength >= distance(arr1[index1].position.x, arr1[index1].position.y, arr2[index2].position.x, arr2[index2].position.y)) {
-            return [arr1[index1].id, arr2[index2].id];
-        }
-        if (arr1[index1].transmissionLength > arr2[index2].transmissionLength) {
-            index2++;
-        }
-        else {
-            index1++;
-        }
-    }
-    throw new Error("no antennas in range");
+ while (index1 < arr1.length && index2 < arr2.length) {
+ if (arr1[index1].transmissionLength + arr2[index2].transmissionLength >= distance(arr1[index1].position.x, arr1[index1].position.y, arr2[index2].position.x, arr2[index2].position.y)) {
+ return [arr1[index1].id, arr2[index2].id];
+ }
+ if (arr1[index1].transmissionLength > arr2[index2].transmissionLength) {
+ index2++;
+ }
+ else {
+ index1++;
+ }
+ }
+ throw new Error("no antennas in range");
 
-}
+ }
+ */
 
 module.exports.sortarray = sortarray;
+//delete it
 module.exports.myread = myread;
 module.exports.checkrange = checkrange;
 module.exports.phase1 = phase1;
 module.exports.phase2 = phase2;
 module.exports.distance = distance;
-
